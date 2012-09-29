6dbc69a2ef4d589b1d2b0c0808ee4f7da3ca2534
tony056 <tony61507@gmil.com>
2012-09-29 23:10:57 +0800

    i don't change anything

--- mainmenu2/gen/tw/android/mainmenu2/R.java
+++ mainmenu2/gen/tw/android/mainmenu2/R.java
@@ -13,10 +13,9 @@ public final class R {
     public static final class drawable {
         public static final int black=0x7f020000;
         public static final int button_style=0x7f020001;
-        public static final int court=0x7f020002;
-        public static final int halfcourt=0x7f020003;
-        public static final int ic_action_search=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
+        public static final int halfcourt=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
         public static final int addscore=0x7f07000d;
